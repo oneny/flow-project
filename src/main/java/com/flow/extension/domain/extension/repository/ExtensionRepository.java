bceb099c3d9476dffca90fc01c076896ae0101ee
oneny <gjwodnjs1996@gmail.com>
2024-01-31 23:25:57 +0900

    Feat: 차단 확장자 조회 mapper 구현

---
+++ src/main/java/com/flow/extension/domain/extension/repository/ExtensionRepository.java
@@ -0,0 +1,15 @@
+package com.flow.extension.domain.extension.repository;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import com.flow.extension.domain.extension.entity.Extension;
+
+@Mapper
+@Repository
+public interface ExtensionRepository {
+
+	List<Extension> findAll();
+}
