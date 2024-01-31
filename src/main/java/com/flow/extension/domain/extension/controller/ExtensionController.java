5170aed96eb441d613a9ce956915c035bcc1d531
oneny <gjwodnjs1996@gmail.com>
2024-01-31 23:38:36 +0900

    Feat: 차단 확장자 조회 controller 추가

--- src/main/java/com/flow/extension/domain/extension/controller/ExtensionController.java
+++ src/main/java/com/flow/extension/domain/extension/controller/ExtensionController.java
@@ -1,13 +1,31 @@
 package com.flow.extension.domain.extension.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import com.flow.extension.domain.extension.entity.Extension;
+import com.flow.extension.domain.extension.service.ExtensionService;
+
+import lombok.RequiredArgsConstructor;
 
 @Controller
+@RequiredArgsConstructor
 public class ExtensionController {
+
+	private final ExtensionService extensionService;
 
 	@GetMapping("/")
 	public String getIndexPage() {
 		return "index";
+	}
+
+	@ResponseBody
+	@GetMapping("/extensions")
+	public List<Extension> selectAllExtensions() {
+		return extensionService.findAll();
 	}
 }
