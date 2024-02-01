ceca80159a0ef04e01258c55963582bc397ad2d4
oneny <gjwodnjs1996@gmail.com>
2024-02-01 12:26:23 +0900

    Feat: DuplicateExtensionException 생성

---
+++ src/main/java/com/flow/extension/domain/extension/exception/DuplicateExtensionException.java
@@ -0,0 +1,8 @@
+package com.flow.extension.domain.extension.exception;
+
+public class DuplicateExtensionException extends RuntimeException {
+
+	public DuplicateExtensionException(String message) {
+		super(message);
+	}
+}
