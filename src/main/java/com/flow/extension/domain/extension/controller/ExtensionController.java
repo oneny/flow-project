7b2441e22cdb0623c0e48ced5000f7264a15c336
oneny <gjwodnjs1996@gmail.com>
2024-02-01 09:33:56 +0900

    Feat: 고정 확장자 등록/삭제 controller 추가

--- src/main/java/com/flow/extension/domain/extension/controller/ExtensionController.java
+++ src/main/java/com/flow/extension/domain/extension/controller/ExtensionController.java
@@ -5,10 +5,14 @@ import static com.flow.extension.global.config.ApiResponse.Status.*;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import com.flow.extension.domain.extension.dto.request.ExtensionRequest;
 import com.flow.extension.domain.extension.entity.Extension;
 import com.flow.extension.domain.extension.service.ExtensionService;
 import com.flow.extension.global.config.ApiResponse;
@@ -32,6 +36,28 @@ public class ExtensionController {
 		return ApiResponse.<List<Extension>>builder()
 			.status(SUCCESS)
 			.result(extensionService.findAll())
+			.build();
+	}
+
+	@ResponseBody
+	@PostMapping("/extensions")
+	public ApiResponse insertExtension(@RequestBody ExtensionRequest extensionRequest) {
+		extensionService.insertExtension(extensionRequest);
+
+		return ApiResponse.builder()
+			.message("성공적으로 추가되었습니다.")
+			.status(SUCCESS)
+			.build();
+	}
+
+	@ResponseBody
+	@DeleteMapping("/extensions")
+	public ApiResponse deleteExtension(@RequestBody ExtensionRequest extensionRequest) {
+		extensionService.deleteExtension(extensionRequest);
+
+		return ApiResponse.builder()
+			.message("성공적으로 삭제되었습니다.")
+			.status(SUCCESS)
 			.build();
 	}
 }
--- src/main/java/com/flow/extension/domain/extension/entity/Extension.java
+++ src/main/java/com/flow/extension/domain/extension/entity/Extension.java
@@ -2,12 +2,16 @@ package com.flow.extension.domain.extension.entity;
 
 import java.time.LocalDateTime;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 @EqualsAndHashCode(of = "id")
 public class Extension {
 
