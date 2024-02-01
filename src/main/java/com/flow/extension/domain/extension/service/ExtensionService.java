8df36a5bfd4d259d4beb13754347662c84dd06ce
oneny <gjwodnjs1996@gmail.com>
2024-02-01 09:33:43 +0900

    Feat: 고정 확장자 등록/삭제 기능 구현

--- src/main/java/com/flow/extension/domain/extension/service/ExtensionService.java
+++ src/main/java/com/flow/extension/domain/extension/service/ExtensionService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.flow.extension.domain.extension.dto.request.ExtensionRequest;
 import com.flow.extension.domain.extension.entity.Extension;
 import com.flow.extension.domain.extension.repository.ExtensionRepository;
 
@@ -19,5 +20,15 @@ public class ExtensionService {
 	@Transactional(readOnly = true)
 	public List<Extension> findAll() {
 		return extensionRepository.findAll();
+	}
+
+	@Transactional
+	public void insertExtension(ExtensionRequest extensionRequest) {
+		extensionRepository.insertExtension(extensionRequest.toEntity());
+	}
+
+	@Transactional
+	public void deleteExtension(ExtensionRequest extensionRequest) {
+		extensionRepository.deleteExtension(extensionRequest.toEntity());
 	}
 }
